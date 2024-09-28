6672888d0251849991e443efd62aaf541a08f045
arleydeziderio <arleydeziderio@gmail.com>
2024-09-27 21:45:42 -0300

    Adding Python Pipeline and Data Files

--- pipeline_00.py
+++ pipeline_00.py
@@ -5,9 +5,11 @@ import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
-folder_url = 'https://drive.google.com/drive/folders/1maqV7E3NRlHp12CsI4dvrCFYwYi7BAAf'
-local_directory = './pasta_gdown'
-
 def download_google_drive_files(folder_url, local_directory):
     os.makedirs(local_directory, exist_ok=True)
     gdown.download_folder(folder_url, output=local_directory, quiet=False, use_cookies=False)
+
+if __name__ == "__main__":
+    folder_url = 'https://drive.google.com/drive/folders/1maqV7E3NRlHp12CsI4dvrCFYwYi7BAAf'
+    local_directory = './gdown_folder'
+    download_google_drive_files(folder_url, local_directory)
